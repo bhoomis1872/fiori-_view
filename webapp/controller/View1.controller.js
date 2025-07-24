7e820f04814b0f627b37fe1f4e09f5a4fa151159
Bhoomi Sharma <bs3683033@gmail.com>
2025-07-24 09:24:52 +0000

    adding fiori training first

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+  ], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+  
+    return Controller.extend("bhoomi.project1.controller.view1", {
+      onInit: function () {
+      
+      }
+    });
+  });
+  
